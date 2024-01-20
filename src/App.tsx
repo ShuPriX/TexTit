5cced19a7c194efddd37a50262703bf6b312d54a
Shubhradeep Paul <shubhradeep612@gmail.com>
2024-01-21 01:34:04 +0530

    all

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./root/RootLayout";
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/lib/validation/index.ts
@@ -0,0 +1,8 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+    name : z.string().min(2,{message: 'too short'}).max(50),
+    username: z.string().min(2,{message: 'Too short'}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8,{message: 'Password must be at least 8 characters.'}).max(50),
+  });
