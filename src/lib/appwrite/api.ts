f2127e3468aff7e99cca5c294b16db2c799dcb25
Shubhradeep Paul <shubhradeep612@gmail.com>
2024-01-21 14:18:57 +0530

    updated-validation

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,6 +1,7 @@
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, avatars } from "./config";
 import { ID } from "appwrite";
+import { string } from "zod";
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -10,9 +11,24 @@ export async function createUserAccount(user: INewUser) {
       user.password,
       user.name
     );
+
+    if(!newAccount) throw new Error("Account creation failed");
+
+      const avatarUrl = avatars.getInitials(user.name);
+
+      // const newUser = await saveUserToDB()
+
     return newAccount;
   } catch (error) {
     console.log(error);
     return error;
   }
 }
+
+
+// export async funiction saveUserToDB(user : {
+//   accountIF ; string;
+//   email : string;
+//   name : string;
+
+// })
